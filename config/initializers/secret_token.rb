0a7c83e5f60cdc45a3b696117901a3003b678857
Vinay <vinay59.grewal@gmail.com>
2013-03-13 00:49:32 +0530

    added article controller and model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Train::Application.config.secret_token = 'ac14c038d4f3840d3115c219baf3e0a2ff215877e1ee010ec38ddf0fc4a5305e694e43527a6cadd756277dc401d8d8f34cfaaf55b2abbf0084c92e1f80d3452e'
